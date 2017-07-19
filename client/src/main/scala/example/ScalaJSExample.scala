03045771390f0a12b77bf1f41bd105fedb8b1356
Pawel Panasewicz <pawel.panasewicz@digital.hmrc.gov.uk>
2017-07-19 14:01:08 +0100

    binded few fields

--- client/src/main/scala/example/ScalaJSExample.scala
+++ client/src/main/scala/example/ScalaJSExample.scala
@@ -1,7 +1,7 @@
 package example
 
 import org.scalajs.dom
-import org.scalajs.dom.html.Pre
+import org.scalajs.dom.html.{Input, Pre}
 import org.scalajs.dom.raw.KeyboardEvent
 import rx.Ctx.Owner
 import rx.Rx.Dynamic
@@ -13,7 +13,7 @@ import scalatags.JsDom
 import scalatags.JsDom.TypedTag
 import scalatags.JsDom.all._
 import scalatags.generic.StylePair
-import scalatags.stylesheet.{ Cls, StyleSheet }
+import scalatags.stylesheet.{Cls, StyleSheet}
 import framework.Framework._
 import rx._
 import wiki.WIKI
@@ -22,22 +22,39 @@ import scalatags.JsDom.all._
 
 object ScalaJSExample {
 
-  object logic {
+  object model {
+    val moneyInputField = Var(0.0)
+    val tax = 0.80
 
-    model.incomeTax.foreach { x =>
-      dom.console.log(x)
+    val ir35CheckBoxIn = Var(false)
+    val marriageCheckBoxIn = Var(false)
+    val pensionReliefCheckboxIn = Var(false)
+    val maintenanceReliefeChecboxIn = Var(false)
+
+    val incomeTax = Rx {
+      moneyInputField() * tax
     }
   }
 
+
   object view {
 
-    val meneyInput = input(placeholder := "0").render
-
-    meneyInput.onkeyup = (x: Any) => {
-      dom.console.log("wordbox on keyup ...")
-      model.in() = meneyInput.value.toDouble
-
+    val meneyInputField: Input = {
+      val i = input(placeholder := "put money here").render
+      i.onkeyup = (x: Any) => {
+        model.moneyInputField() = i.value.toDouble
+      }
+      i
     }
+
+    val ir35InputField: Input = {
+      val i = input(`type` := "checkbox").render
+      i.onchange = (x: Any) => {
+        model.ir35CheckBoxIn() = i.checked
+      }
+      i
+    }
+
     //
 
     val earningsAfterInitialTax = input().render
@@ -45,15 +62,18 @@ object ScalaJSExample {
     val taxToPayOutput = input(readonly, value := model.incomeTax)
     val corporationTax = input(readonly, value := model.incomeTax)
     val nationalInsurance = input(readonly, value := model.incomeTax)
+
+
+
     val calc = div(
       p("Income/Earnings"),
-      meneyInput,
+      meneyInputField,
       p("Earnings after income tax deductions and NI"),
       earningsAfterInitialTax,
       p(""),
       span(
         style := "display: inline",
-        WIKI.main("IR35", label(WIKI.infoItem("IR35", WIKI.wikiservice.keys.IR35), input(`type` := "checkbox")).render),
+        WIKI.main("IR35", label(WIKI.infoItem("IR35", WIKI.wikiservice.keys.IR35), ir35InputField).render),
         WIKI.main("Marriage allowance", label(WIKI.infoItem("Marriage allowance", WIKI.wikiservice.keys.marriageAllowance), input(`type` := "checkbox")).render),
         WIKI.main("Pension Relief", label(WIKI.infoItem("Pension Relief", WIKI.wikiservice.keys.pensionRelief), input(`type` := "checkbox")).render),
         WIKI.main("Maintenance Relief", label(WIKI.infoItem("Maintenance relief", WIKI.wikiservice.keys.maintenanceRelief), input(`type` := "checkbox")).render)
@@ -70,6 +90,13 @@ object ScalaJSExample {
           h2(
             "Earnings After Tax Deductions",
             nationalInsurance
+          ),
+
+          div(
+            cls := "debug-pane",
+            hidden, //comment it to hide debug
+            p("ir35 = ", model.ir35CheckBoxIn.map(_.toString())),
+            p("married = ", model.marriageCheckBoxIn.map(_.toString()))
           )
         )
       )
@@ -77,13 +104,6 @@ object ScalaJSExample {
 
   }
 
-  object model {
-    val in = Var(0.0)
-    val tax = 0.80
-    val incomeTax = Rx {
-      in() * tax
-    }
-  }
 
   val main = view.calc.render
 
